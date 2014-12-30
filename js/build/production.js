7192479d15d1924616072228020b55ef7d866748
quattromani <max@maxquattromani.com>
2014-12-30 11:22:56 -0700

    update to working

--- js/build/production.js
+++ js/build/production.js
@@ -562,3 +562,30 @@ $('.has-tooltip').tooltips();
 }(jQuery));
 
 $('.card-component').cardComponent();
+(function($) {
+
+  $.fn.cardComponent = function() {
+    $('.reveal-details').each(function() {
+
+      var cardComponent = jQuery(this);
+			
+      cardComponent.click(function() {
+        $(".card-component section").slideToggle( "normal", function() {
+				// animation complete
+  			});
+  			
+  			// toggle button text
+  			cardComponent.toggleClass('show');
+		    if (cardComponent.hasClass('show')) {
+		        cardComponent.text('Hide Details');
+		    } else {
+		        cardComponent.text('Show Details');
+		    }
+      });
+
+    });
+  }
+
+}(jQuery));
+
+$('.card-component').cardComponent();
---
+++ sandbox/nav/index.md
@@ -0,0 +1,33 @@
+---
+layout: default
+title: Card
+---
+
+<div class="row">
+	<div class="third">
+		<div class="card-component">
+			
+			<header>
+				Information Card
+			</header>
+			
+			<section>
+				<img class="left image-circular" src="images/rogerheaston.jpg">
+				<address>
+					<strong>Roger Heaston</strong><br>
+					1390 Lawrence St.<br>
+					Denver, CO 80204<br>
+					<a class="tel" href="tel:+3032962413">(303) 296-2413</a><br>
+					<a href="mailto:roger@placewise.com">roger@placewise.com</a>
+				</address>
+			</section>
+			
+			<footer>
+				<button class="btn btn-primary reveal-details">
+				  Show Details
+				</button>
+			</footer>
+			
+		</div>
+	</div>
+</div>
