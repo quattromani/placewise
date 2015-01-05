698233ed4af361c59adc2ff20ab7374e793801b3
quattromani <max@maxquattromani.com>
2015-01-05 16:26:05 -0700

    added rich-media component

--- js/build/production.js
+++ js/build/production.js
@@ -582,3 +582,18 @@ $('.card-component').cardComponent();
 }(jQuery));
 
 $('.card-component').cardComponent();
+(function($) {
+
+  $.fn.richMedia = function() {
+    $('.rich-media').each(function() {
+      $('.media-close').click(function(e) {
+        $(this).siblings('section').slideToggle();
+        $(this).toggleClass('open');
+        e.preventDefault();
+      });
+    });
+  }
+
+}(jQuery));
+
+$('.rich-media').richMedia();
--- sandbox/rich-media/js/card-component.js
+++ sandbox/rich-media/js/card-component.js
@@ -2,7 +2,11 @@
 
   $.fn.richMedia = function() {
     $('.rich-media').each(function() {
-
+      $('.media-close').click(function(e) {
+        $(this).siblings('section').slideToggle();
+        $(this).toggleClass('open');
+        e.preventDefault();
+      });
     });
   }
 
